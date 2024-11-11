a1a261b49ad8212f6c8f5fbcd88c8cbb4ef6587c
Ar1ocker <ar1oc@yandex.ru>
2024-11-11 15:56:18 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SquadJS-Disband-Squad-By-Name
 The plugin for disband squads by its name
+
+The plugin can be used to protect against squads with an obscene name or just a forbidden name.
---
+++ disband-squad-by-name.js
@@ -0,0 +1,73 @@
+import BasePlugin from "./base-plugin.js";
+
+export default class DisbandSquadByName extends BasePlugin {
+  static get description() {
+    return "The plugin for disband squad by his name";
+  }
+
+  static get defaultEnabled() {
+    return false;
+  }
+
+  static get optionsSpecification() {
+    return {
+      names: {
+        required: true,
+        description: "The list of squad name regex",
+        default: [
+          {
+            regexes: ["", ""],
+            messages: ["message 1", "message 2"],
+          },
+          {
+            regexes: ["", ""],
+            messages: ["message 1", "message 2"],
+          },
+        ],
+      },
+      message_frequency: {
+        required: false,
+        description: "The message frequency",
+        default: 5,
+      },
+    };
+  }
+
+  constructor(server, options, connectors) {
+    super(server, options, connectors);
+
+    this.warns = this.warns.bind(this);
+    this.checkSquadName = this.checkSquadName.bind(this);
+  }
+
+  async mount() {
+    this.server.on("SQUAD_CREATED", async (data) => {
+      if (data.player) {
+        this.checkSquadName(data.squadName.toLowerCase().trim(), data.player);
+      }
+    });
+  }
+
+  async checkSquadName(squadName, player) {
+    for (const { regexes, messages } of this.options.names) {
+      for (const regex of regexes) {
+        if (squadName.match(regex)) {
+          await Promise.all([
+            this.server.rcon.execute(`AdminDisbandSquad ${player.teamID} ${player.squadID}`),
+            this.warns(player.eosID, messages, this.options.message_frequency),
+          ]);
+        }
+      }
+    }
+  }
+
+  async warns(playerID, messages, frequency = 5) {
+    for (const [index, message] of messages.entries()) {
+      await this.server.rcon.warn(playerID, message);
+
+      if (index != messages.length - 1) {
+        await new Promise((resolve) => setTimeout(resolve, frequency * 1000));
+      }
+    }
+  }
+}
